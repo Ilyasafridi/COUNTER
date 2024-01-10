fec694a22438b9b355fe0e6f85381dc18cb19795
Ilyasafridi <abidullahafridi936@gmail.com>
2024-01-10 09:34:04 -0800

    COUNTER

---
+++ counter.js
@@ -0,0 +1,10 @@
+let count = 0;
+
+function incre () {
+    count++
+    document.getElementById('tnum').innerHTML = count;
+}
+function reset () {
+    count = 0;
+    document.getElementById('tnum').innerHTML = count;
+}
